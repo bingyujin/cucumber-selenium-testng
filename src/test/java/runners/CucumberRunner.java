51b18db2a36eb38c0c9de296f834ffc4daf05de9
bingyujin <41620768+bingyujin@users.noreply.github.com>
2024-02-29 15:10:50 +0800

    Update CucumberRunner.java

--- src/test/java/runners/CucumberRunner.java
+++ src/test/java/runners/CucumberRunner.java
@@ -1,7 +1,9 @@
 package runners;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
+//import cucumber.api.CucumberOptions;
+//import cucumber.api.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
 											features = "src/test/resources/features", 
